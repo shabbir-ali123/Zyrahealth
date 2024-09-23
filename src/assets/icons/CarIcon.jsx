f08570d82c3a7d2b436bda7f2411e3afd65b8d1e
shabbir ali <shamilak936@gmail.com>
2024-09-23 20:44:20 +0500

    right in home

---
+++ src/assets/icons/CarIcon.jsx
@@ -0,0 +1,19 @@
+
+import React from 'react';
+
+const CarIcon = () => (
+
+
+    <svg width="30" height="30" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M28.4 8.40077L25.4667 11.3341L23.2667 5.90744C23.0592 5.35207 22.6883 4.8726 22.2029 4.53223C21.7175 4.19186 21.1403 4.00656 20.5475 4.00077H9.92C9.32234 3.98705 8.73475 4.15626 8.23593 4.48575C7.73711 4.81524 7.35087 5.28928 7.12893 5.84437L4.93333 11.3341L2 8.40077" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M7.86719 17.2012H7.88052" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M22.5352 17.2012H22.5485" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M25.4667 11.333H4.93333C3.3133 11.333 2 12.6463 2 14.2663V20.133C2 21.753 3.3133 23.0663 4.93333 23.0663H25.4667C27.0867 23.0663 28.4 21.753 28.4 20.133V14.2663C28.4 12.6463 27.0867 11.333 25.4667 11.333Z" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M4.92969 23.0654V25.9988" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+        <path d="M25.4648 23.0654V25.9988" stroke="#1C1C1C" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" />
+    </svg>
+
+
+);
+
+export default CarIcon;
--- src/data/CardsData.jsx
+++ src/data/CardsData.jsx
@@ -1,5 +1,8 @@
 // src/data/cardsData.js
 import PersonalCareIcon from '../assets/icons/PersonalCareIcon';
+import HomeMaintance from '../assets/icons/HomeMaintance';
+import CarIcon from '../assets/icons/CarIcon';
+import MovingIcon from '../assets/icons/MovingIcon';
 // import Icon2 from '../icons/Icon2';
 // import Icon3 from '../icons/Icon3';
 // Import other icons as needed
@@ -10,20 +13,65 @@ const cardsData = [
     title: 'Personal Care',
     description: 'Bathing, dressing, meal prep, companionship, and more',
     Icon: PersonalCareIcon,
+    bgColor: 'bg-[#BFB5FE]'
   },
   {
     id: 2,
-    title: 'Housekeeping',
-    description: 'Cleaning, laundry, and organization services',
-    Icon: PersonalCareIcon,
+    title: 'Home maintenance',
+    description: 'Home repairs, mods, appliance, electrical, and more',
+    Icon: HomeMaintance,
+    bgColor: 'bg-[#FEBCD5]'
   },
   {
     id: 3,
-    title: 'Transportation',
-    description: 'Assistance with travel and errands',
-    Icon: PersonalCareIcon,
+    title: 'Non-medical transportation',
+    description: 'Appointments, shopping, and errands',
+    Icon: CarIcon,
+    bgColor: 'bg-[#A9EEB8]'
   },
-  // Add more card objects up to 10
+  {
+    id: 3,
+    title: 'Moving and storage',
+    description: 'Downsizing, packing, relocation, and storage solutions',
+    Icon: MovingIcon,
+    bgColor: 'bg-[#9DEFEB]'
+  },
+  {
+    id: 3,
+    title: 'Wellness',
+    description: 'Mobile massage, social activities, community engagement, holistic care',
+    Icon: PersonalCareIcon,
+    bgColor: 'bg-[#F1AAF2]'
+  },
+  {
+    id: 3,
+    title: 'Medical equipment',
+    description: 'Mobility aids, monitoring devices, safety installations',
+    Icon: PersonalCareIcon,
+    bgColor: 'bg-[#AADCF2]'
+  },
+  {
+    id: 3,
+    title: 'Professional services',
+    description: 'Taxes, retirement and estate planning, mobile notary, wills, trusts, and more',
+    Icon: PersonalCareIcon,
+    bgColor: 'bg-[#F2BBAA]'
+  },
+  {
+    id: 3,
+    title: 'Pet care',
+    description: 'Feeding, walking, grooming, and veterinary care',
+    Icon: PersonalCareIcon,
+    bgColor: 'bg-[#D7F2AA]'
+  },
+  {
+    id: 3,
+    title: 'End-of-life planning',
+    description: 'Hospice care and funeral arrangements',
+    Icon: PersonalCareIcon,
+    bgColor: 'bg-[#FCDD8E]'
+  },
+
 ];
 
 export default cardsData;
