8bbcc8f5dd5c4d780804976d1665917cc9612ff7
ajmalkarim1 <ajmalkareem0099@gmail.com>
2024-09-23 09:18:20 -1200

    add a header

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import "./App.css";
 import HomeCardsContainer from "./components/HomeCardsContainer";
 import HeroWrapper from "./components/HeroWrapper";
 import Header from "./layout/Header";
+import HomeSection from "./components/Hero";
 function App() {
   return (
     <>
       <Header />
+      <HomeSection />
       <HeroWrapper />
       <HomeCardsContainer />
     </>
