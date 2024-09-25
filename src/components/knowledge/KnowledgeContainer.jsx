353dd584f704844438c7f0c0d1d1a35baef9bb99
shabbir ali <shamilak936@gmail.com>
2024-09-25 16:06:33 +0500

    ui

--- src/components/knowledge/KnowledgeContainer.jsx
+++ src/components/knowledge/KnowledgeContainer.jsx
@@ -7,7 +7,7 @@ function KnowledgeContainer() {
   return (
     <>
     <div className="bg-[#F7F7F7]">
-    <div className="mx-20  py-20 mt-20	">
+    <div className=" mx-4 lg:mx-20  py-20 mt-20	">
         <div className="text-center">
           <h2 className="font-semibold text-4xl">Your knowledge hub</h2>
           <div className="mx-auto w-[100px] h-[3px] bg-[#1C1C1C] mt-2"></div>
